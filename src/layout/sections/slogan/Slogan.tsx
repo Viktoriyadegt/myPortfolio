ad954038b5821228ff1cd2ee93cf834495d6b1e2
Viktoriyadegt <vika.degtyareva.12@mail.ru>
2024-10-03 19:47:12 +0300

    slogan

---
+++ src/layout/sections/slogan/Slogan.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle";
+import {Button} from "../../../components/Button";
+
+export const Slogan = () => {
+    return (
+        <StyledSlogan>
+            <SectionTitle>I Am Available For Freelance</SectionTitle>
+            <Button>Hire me</Button>
+        </StyledSlogan>
+    );
+};
+
+const StyledSlogan = styled.section`
+    min-height: 30vh;
+    background-color: #daa8c0;
+    
+    align-items: center;   //не по плану
+    display: flex;
+    flex-direction: column;
+   
+    button {
+        width: 170px;
+    }
+`
